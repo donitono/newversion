146c06d1af739ba5197a29b74be49c711f3e782d
donitono <bangabykah@gmail.com>
2025-08-12 13:14:27 -0700

    Add loader files for easy script execution

---
+++ loader.lua
@@ -0,0 +1,11 @@
+--[[
+    XSAN Fish It Pro Ultimate v2.0 - Quick Loader
+    
+    Simple loader for XSAN Fish It Pro Ultimate Modern UI
+    Just copy and paste this into your executor!
+--]]
+
+print("🎣 XSAN Fish It Pro Ultimate v2.0 - Loading...")
+
+-- Load the main script
+loadstring(game:HttpGet('https://raw.githubusercontent.com/donitono/newversion/master/main_modern.lua'))()
---
+++ quick_load.lua
@@ -0,0 +1,2 @@
+-- XSAN Fish It Pro Ultimate v2.0 Modern UI
+loadstring(game:HttpGet('https://raw.githubusercontent.com/donitono/newversion/master/main_modern.lua'))()
